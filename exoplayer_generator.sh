75c6408f5e393813962988357cca23eaaaa6460c
Davide Bertola <dade@dadeb.it>
2021-05-11 22:18:27 +0200

    update exoplayer generator script

--- exoplayer_generator.sh
+++ exoplayer_generator.sh
@@ -22,33 +22,10 @@ find . -type f -name "*.bak" -delete
 # 4. move package folders (ignore errors that are printed in the console)
 find . -path "*/com/google" -exec bash -c "mv {}/ \`echo  {} | sed 's/com\/google/com\/deltatre/g' \`" \;
 
-# 5. pushing artifacts to bintray on https://bintray.com/google/exoplayer
-export USERNAME=davibe
-export APIKEY=5836dee73f8cd3d4e7c3e46b9c3c4bff38ef4694
-
-test -n $USERNAME || echo 'Missing username for bintray upload (USERNAME env var)'
-test -n $APIKEY || echo 'Missing apikey for bintray upload (APIKEY env var)'
-
-./gradlew build
-./gradlew bintrayUpload \
-    -PbintrayUser=$USERNAME \
-    -PbintrayKey=$APIKEY \
-    -PdryRun=false \
-    -x releaseAndroidJavadocs \
-    --continue
+# 5. pushing artifacts to repo/ golfrt1
+./gradlew publish
 
 # 6. Do not commit changes, just reverte them.
 git clean -fdx
 git reset --hard head
-# this ensures we can always start from source that closely match the official exoplayer repository
 
-# TODOS:
-
-- publish on github
-- publish on bintray
-- add thedivaproduct accounts
-- add to shared accounts file
-- test in TestApp
-- test in integrationdemo
-- test with exoplayer 2.8 present
-- test running exoplayer 2.8 together
